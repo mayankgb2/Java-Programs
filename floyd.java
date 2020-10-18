cb258586a0578d39412032db801b33f51065e2a2
lakshay-droid <57001958+lakshay-droid@users.noreply.github.com>
2020-10-18 10:19:22 +0530

    Added Floyd triangle

---
+++ floyd.java
@@ -0,0 +1,18 @@
+#https://www.facebook.com/lakshay.gulati.5/posts/3439658002768650
+# subscribed by Lakshay
+public class floyd {
+
+    public static void main(String[] args) {
+        int x=1;
+        for (int j =1;j<=5;j++){
+                for (int i=0;i<j;i++){
+                    System.out.print(" "+x);
+                    x++;
+
+                    }
+                System.out.println();
+
+        }
+
+    }
+}
