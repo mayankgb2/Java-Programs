78d721bb04dcfc829f140f51ae7e3c43ecc77b75
Janvit __ jp <64550298+jyothiprakashpanaik@users.noreply.github.com>
2020-10-21 11:07:01 +0530

    Added beep_sound generator in java
    #https://www.facebook.com/jyothiprakash.patnaikuni/posts/107593291135289
    # subscribed by jyothiprakash patnaik

---
+++ Beep_sound.java
@@ -0,0 +1,9 @@
+#https://www.facebook.com/jyothiprakash.patnaikuni/posts/107593291135289
+# subscribed by jyothiprakash patnaik
+import java.awt.*;
+public class Beep_sound {
+    
+    public static void main(String[] args) {
+        Toolkit.getDefaultToolkit().beep();
+    }
+}
