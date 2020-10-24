cc85e6e016e406748dd169710c8a3e173f1042f0
Nikita Desale <32368088+nikitadesale@users.noreply.github.com>
2020-10-24 01:42:06 -0600

    Added binary to decimal program

---
+++ binarytodecimal.java
@@ -0,0 +1,33 @@
+
+#https://www.facebook.com/100028679802914/posts/449717249327598/
+#subcribed by Nikita Desale
+class GFG {
+    static int binaryToDecimal(int n)
+    {
+        int num = n;
+        int dec_value = 0;
+ 
+        // Initializing base
+        // value to 1, i.e 2^0
+        int base = 1;
+ 
+        int temp = num;
+        while (temp > 0) {
+            int last_digit = temp % 10;
+            temp = temp / 10;
+ 
+            dec_value += last_digit * base;
+ 
+            base = base * 2;
+        }
+ 
+        return dec_value;
+    }
+ 
+    // Driver Code
+    public static void main(String[] args)
+    {
+        int num = 10101001;
+        System.out.println(binaryToDecimal(num));
+    }
+}
